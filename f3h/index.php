35c6869f99041ba3aaabcaa2e09d612720e06412
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-03-28 21:35:09 +0700

    Update

--- f3h/index.php
+++ f3h/index.php
@@ -1,4 +1,7 @@
 <?php
 
-Asset::set(__DIR__ . D . 'lot' . D . 'asset' . D . 'index.min.js', 10);
+$z = defined('TEST') && TEST ? '.' : '.min.';
+
+Asset::set(__DIR__ . D . 'lot' . D . 'asset' . D . 'index' . $z . 'js', 10);
+
 Asset::script("window.F3H && window.F3H.init && window.F3H.init(" . To::JSON($state->x->f3h->sources ?? 'body') . ", " . To::JSON($state->x->f3h->state ?? []) . ")", 10);
