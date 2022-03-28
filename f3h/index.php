33cca1d200e0586179c671e3c5a3df917bdab6ad
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-03-28 22:51:01 +0700

    Short Asset Path

--- f3h/index.php
+++ f3h/index.php
@@ -1,7 +1,4 @@
 <?php
 
-$z = defined('TEST') && TEST ? '.' : '.min.';
-
-Asset::set(__DIR__ . D . 'lot' . D . 'asset' . D . 'index' . $z . 'js', 10);
-
+Asset::set(__DIR__ . D . 'index' . (defined('TEST') && TEST ? '.' : '.min.') . 'js', 10);
 Asset::script("window.F3H && window.F3H.init && window.F3H.init(" . To::JSON($state->x->f3h->sources ?? 'body') . ", " . To::JSON($state->x->f3h->state ?? []) . ")", 10);
